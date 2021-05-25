45bd4360a327af7aa99ba0c4008aff8d5e382b38
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2021-05-25 11:23:31 +0100

    removed new file

--- new_file.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Calculate the mean of an array
-def calculate_mean(array):
-    return np.mean(array)
-
-# Calculate std deviation
-def calculate_std(array):
-    return np.std(array)
