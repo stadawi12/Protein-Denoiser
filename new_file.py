4aa07a22d707280ae9b02d08eec6d0772e586984
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2021-05-25 11:22:11 +0100

    Added new function

--- new_file.py
+++ new_file.py
@@ -3,3 +3,7 @@ import numpy as np
 # Calculate the mean of an array
 def calculate_mean(array):
     return np.mean(array)
+
+# Calculate std deviation
+def calculate_std(array):
+    return np.std(array)
