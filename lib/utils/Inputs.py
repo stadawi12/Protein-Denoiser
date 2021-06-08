7c0e8b0d638a4f71cb958a847bc6f99a88107229
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2021-06-08 15:21:43 +0100

    Added parser options for train, test, proc

--- lib/utils/Inputs.py
+++ lib/utils/Inputs.py
@@ -1,4 +1,5 @@
 import yaml
+import argparse
 
 def Read_Input(input_path : str):
 
@@ -6,3 +7,15 @@ def Read_Input(input_path : str):
         input_data = yaml.load(input_file, Loader=yaml.FullLoader)
 
     return input_data
+
+def Create_Parser():
+
+    MSG_ACTION = "Select an action to perform on the network"
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-a', '--action', type = str,
+            choices = ['train', 'test', 'proc'],
+            help = MSG_ACTION)
+
+    return parser.parse_args()
--- main.py
+++ main.py
@@ -5,9 +5,24 @@ sys.path.insert(1, 'lib/utils/ml_toolbox/src')
 sys.path.insert(1, 'lib/net')
 
 from Train import Train
-from Inputs import Read_Input
+from Inputs import Read_Input, Create_Parser
 import unet
 
-input_data = Read_Input('inputs.yaml')
 
-Train(unet, input_data, 'data')
+if __name__ == '__main__':
+
+    args = Create_Parser()
+
+    input_data = Read_Input('inputs.yaml')
+
+    if args.action == 'train':
+
+        Train(unet, input_data, 'data')
+
+    if args.action == 'test':
+
+        print("Sorry, come back when this has been developed!")
+
+    if args.action == 'proc':
+
+        print("Sorry, come back when this has been developed!")
