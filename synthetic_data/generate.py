e3f5a2e89b3410c46bd4c61bbebf1292de00fe35
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2021-06-23 14:42:38 +0100

    If map already generate, dont generate again

--- synthetic_data/generate.py
+++ synthetic_data/generate.py
@@ -6,18 +6,26 @@ from Inputs import Read_Input
 
 if __name__ == '__main__':
 
-    print("Generating density map from model...")
-
+    # Read inputs
     input_data = Read_Input('inputs.yaml')
 
     pdb = input_data['pdb']
     r   = input_data['r']
 
-    pdb_path = 'pdbs'
-    p = os.path.join('maps', str(r))
+    # Check if map already generated
+    mrc = pdb[:-3] + 'mrc'
+    check_path = os.path.join('maps', str(r), mrc)
+    path_exists = os.path.exists(check_path)
+    if not path_exists:
+        print("Generating density map from model...")
+
+        pdb_path = 'pdbs'
+        p = os.path.join('maps', str(r))
 
 
-    s = "chimera --nogui --script 'chimera_molmap.py %s %d %s.mrc' > /dev/null" % (
-        os.path.join(pdb_path, pdb), float(r),
-        os.path.join(p, pdb[:-4]))
-    os.system(s)
+        s = "chimera --nogui --script 'chimera_molmap.py %s %d %s.mrc' > /dev/null" % (
+            os.path.join(pdb_path, pdb), float(r),
+            os.path.join(p, pdb[:-4]))
+        os.system(s)
+    else: 
+        print("Maps has already been generated")
